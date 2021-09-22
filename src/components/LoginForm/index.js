d5cb4506565ee32b4b1b6e21ab7818dad6c6d91d
VaillantA <antoine-vaillant@hotmail.com>
2021-09-22 10:28:31 +0200

    feat/Registration-Form, From create --DOING

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,13 +1,62 @@
-
+import { useEffect } from 'react';
+import Ripple
 // import './style.scss';
 
 const LoginForm = () => {
+  useEffect(() => {
+    document.title = 'Registration Form' 
+  }, []);
+
   return(
-    <h1>COucou depuis Le form</h1>
+    <div className="container">
+      <div className="title">
+          Registration Form
+      </div>
+      <div className="form">
+          <div className="input-field">
+              <label>First Name</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Last Name</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Password</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Confirm Password</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Confirm Password</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Gender</label>
+              <div className="gender-select">
+                <select>
+                  <option value="">Select</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+          </div>
+          <div className="input-field">
+              <label>Email Adress</label>
+              <input type="text" className="input"/>
+          </div>
+          <div className="input-field">
+              <label>Discription</label>
+              <textarea type="text" className="input"> </textarea>
+          </div>
+          <button type="button" className="button">Register</button>
+      </div>
+    </div>
   )
 }
 
 
 
 export default LoginForm;
-

4af9438d7f7bed4161ad43bc0b28f3525d958feb
VaillantA <antoine-vaillant@hotmail.com>
2021-09-27 18:33:39 +0200

    add dist

--- config/paths.js
+++ config/paths.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  root: path.resolve(__dirname, '../'), // source files
+  root: path.resolve(__dirname, '../dist'), // source files
   src: path.resolve(__dirname, '../src'), // source files
   assets: path.resolve(__dirname, '../src/assets'), // assets files
   build: path.resolve(__dirname, '../dist'), // production build files

494c8fd6b9485a1afe06e3386b2a35662e3ff152
claire driencourt <cdriencourt@hotmail.fr>
2021-10-06 12:13:40 +0200

    feat: formulaire de contact DONE

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,36 @@
+import './style.scss';
 
 const Contact = () => (
-  <div className="contact">
-    Coucou depuis contact
+  <div className="contactPage">
+    <h1 className="contactPage--text">
+      Contactez-nous
+    </h1>
+    <form
+      className="contactForm"
+      action="https://formsubmit.co/19238f78652c8cf9a7d562f7a73b79d6"
+      method="POST">
+      <input
+        className="contactForm--nameInput"
+        type="text"
+        name="name"
+        placeholder="Entrez votre nom"
+        required
+      />
+      <textarea
+        className="contactForm--messageInput"
+        rows="5"
+        type="message"
+        name="message"
+        placeholder="Entrez votre message"
+        required
+      />
+      <button
+        className="contactForm--button"
+        type="submit"
+      >
+        Send
+      </button>
+    </form>
   </div>
 );
 

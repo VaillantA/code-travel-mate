271cfd408c184fcb4e80bc5b723a767f6f577415
Siré Sonko <kahddy@gmail.com>
2021-10-08 22:30:55 +0200

    feat: css about us --DONE

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -7,42 +7,52 @@ import claire from 'src/assets/Image/claire.png';
 
 const AboutUs = () => (
   <div className="about-us">
-    <h1>Meet the team</h1>
+    <h1 className="about-us--tittle">Meet the team</h1>
     <div className="peoples">
       <div className="person">
-        <h2>Benjamin</h2>
         <div className="div-image">
           <img src={ben} alt="" className="image" />
         </div>
-        <h3>Product Owner / Lead developer back</h3>
+        <div className="person-text">
+        <h2 className="person-name">Benjamin</h2>
+        <h3 className="tittle-work">Product Owner / Lead developer back</h3>
+        </div>
       </div>
       <div className="person">
-        <h2>Siré</h2>
         <div className="div-image">
           <img src={sire} alt="" className="image" />
         </div>
+        <div className="person-text">
+        <h2 className="person-name">Siré</h2>
         <h3>Scrum Master</h3>
+        </div>
       </div>
       <div className="person">
-        <h2>Tai</h2>
         <div className="div-image">
           <img src={tai} alt="" className="image" />
         </div>
+        <div className="person-text">
+        <h2 className="person-name">Tai</h2>
         <h3>Git Master</h3>
+        </div>
       </div>
       <div className="person">
-        <h2>Claire</h2>
         <div className="div-image">
           <img src={claire} alt="" className="image" />
         </div>
+        <div className="person-text">
+        <h2 className="person-name">Claire</h2>
         <h3>Référente technique / Lead designer</h3>
+        </div>
       </div>
       <div className="person">
-        <h2>Antoine</h2>
         <div className="div-image">
           <img src={antoine} alt="" className="image" />
         </div>
+        <div className="person-text">
+        <h2 className="person-name">Antoine</h2>
         <h3>Lead developer front</h3>
+        </div>
       </div>
 
     </div>

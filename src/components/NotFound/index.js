8672956cf984b0636900585e1ef7a09893b9847e
VaillantA <antoine-vaillant@hotmail.com>
2021-10-08 10:37:31 +0200

    feat:feature/Error in progress

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import anais from 'src/assets/Image/anais.png';
+import fabio from 'src/assets/Image/fabio.png';
+import alexis from 'src/assets/Image/alexis.png';
+
 // == Import
 import './style.scss';
 
@@ -11,7 +15,12 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1>Page non trouvée</h1>
-      <p>Désolé cette page n'existe pas</p>
+      <p className="notfound">Désolé cette page n'existe pas</p>
+      <div className="images">
+        <img src={fabio} alt="" className="image" />
+        <img src={anais} alt="" className="image" />
+        <img src={alexis} alt="" className="image" />
+      </div>
       <p><Link className="not-found-link" to="/">Retourner à l'accueil</Link></p>
     </div>
   );
@@ -19,4 +28,3 @@ const NotFound = () => {
 
 // == Export
 export default NotFound;
-

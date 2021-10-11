6101dcf58ab8e24b5102329b4c21edd2a6336578
VaillantA <antoine-vaillant@hotmail.com>
2021-10-11 14:11:31 +0200

    feat: about us --DONE

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -7,52 +7,41 @@ import claire from 'src/assets/Image/claire.png';
 
 const AboutUs = () => (
   <div className="about-us">
-    <h1 className="about-us--tittle">Meet the team</h1>
+    <h1 className="about-title">Meet the team</h1>
     <div className="peoples">
+
       <div className="person">
         <div className="div-image">
           <img src={ben} alt="" className="image" />
         </div>
-        <div className="person-text">
-        <h2 className="person-name">Benjamin</h2>
-        <h3 className="tittle-work">Product Owner / Lead developer back</h3>
-        </div>
+        <h3 className="text"> Benjamin: Product Owner / Lead developer back</h3>
       </div>
+
       <div className="person">
         <div className="div-image">
           <img src={sire} alt="" className="image" />
         </div>
-        <div className="person-text">
-        <h2 className="person-name">Siré</h2>
-        <h3>Scrum Master</h3>
-        </div>
+        <h3 className="text"> Siré: Scrum Master</h3>
       </div>
+
       <div className="person">
         <div className="div-image">
           <img src={tai} alt="" className="image" />
         </div>
-        <div className="person-text">
-        <h2 className="person-name">Tai</h2>
-        <h3>Git Master</h3>
-        </div>
+        <h3 className="text"> Tai: Git Master</h3>
       </div>
       <div className="person">
+  
         <div className="div-image">
           <img src={claire} alt="" className="image" />
         </div>
-        <div className="person-text">
-        <h2 className="person-name">Claire</h2>
-        <h3>Référente technique / Lead designer</h3>
-        </div>
+        <h3 className="text"> Claire: Technical referent / Lead designer</h3>
       </div>
       <div className="person">
         <div className="div-image">
           <img src={antoine} alt="" className="image" />
         </div>
-        <div className="person-text">
-        <h2 className="person-name">Antoine</h2>
-        <h3>Lead developer front</h3>
-        </div>
+        <h3 className="text">Antoine: Lead developer front</h3>
       </div>
 
     </div>

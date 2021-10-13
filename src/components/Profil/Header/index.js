01e9d3bb5d8b396e38162acb01f90b90cb7e2c4d
Siré Sonko <kahddy@gmail.com>
2021-10-13 09:52:21 +0200

    feat:develop,modification of the profil titles

--- src/components/Profil/Header/index.js
+++ src/components/Profil/Header/index.js
@@ -66,7 +66,7 @@ const Header = (id) => {
               clipRule="evenodd" 
               d="M45 1.5H3c-.8 0-1.5.7-1.5 1.5v42c0 .8.7 1.5 1.5 1.5h42c.8 0 1.5-.7 1.5-1.5V3c0-.8-.7-1.5-1.5-1.5zm-40.5 3h11v11h-11v-11zm0 14h11v11h-11v-11zm11 25h-11v-11h11v11zm14 0h-11v-11h11v11zm0-14h-11v-11h11v11zm0-14h-11v-11h11v11zm14 28h-11v-11h11v11zm0-14h-11v-11h11v11zm0-14h-11v-11h11v11z"
                fillRule="evenodd" /></svg>
-              <p className="navItem-text">MES EVENEMENTS </p>
+              <p className="navItem-text">EVENTS </p>
             </button>
           </li>
           <li className={favoritesList ? 'navItem active' : 'navItem'}>
@@ -75,7 +75,7 @@ const Header = (id) => {
               <path 
               d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" />
               </svg>
-              <p className="navItem-text">FAVORIS</p>
+              <p className="navItem-text">EVENTS CREATED</p>
             </button>
           </li>
           <li className="navItem">

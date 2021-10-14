2cf95ff2b5dec806bf0451a5fb63943cbd6b33d8
claire driencourt <cdriencourt@hotmail.fr>
2021-10-14 10:23:26 +0200

    load doing

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,10 +3,7 @@ import React from 'react';
 import './style.scss';
 
 const Loading = () => (
-  <div className="loading">
-    <span style="font-size: 3em; color: Tomato;"></span>
-    <i className="fas fa-globe-africa" />
-  </div>
+  <div className="loading">Veuillez patienter</div>
 );
 
 export default Loading;

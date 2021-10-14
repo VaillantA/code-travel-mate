2dcc609d9e8abe04405ca8a0fbc5bde85226afa1
claire driencourt <cdriencourt@hotmail.fr>
2021-10-14 08:55:34 +0200

    correction css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -64,7 +64,6 @@ const App = () => {
   return (
     <div className="app">
       <NavBar />
-      <Loading />
       <Settings />
       {/* <EventForm /> */}
       <Switch>
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './style.scss';
 
 const Loading = () => (
-  <div className="loading">Veuillez patienter</div>
+  <div className="loading">
+    <i className="fas fa-globe-africa" />
+  </div>
 );
 
 export default Loading;

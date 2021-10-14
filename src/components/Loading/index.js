c2502281db52970b0ed891745e0c13aed1a631ae
claire driencourt <cdriencourt@hotmail.fr>
2021-10-14 09:57:22 +0200

    CSS hover OK

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import './style.scss';
 
 const Loading = () => (
-  <div className="loading">Veuillez patienter</div>
+  <div className="loading">
+    <span style="font-size: 3em; color: Tomato;"></span>
+    <i className="fas fa-globe-africa" />
+  </div>
 );
 
 export default Loading;
